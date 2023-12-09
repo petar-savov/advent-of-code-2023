27160a4426f91a00e2bd9ff1a5034bbf67f774b1
petar-savov <p.savov@outlook.com>
2023-12-09 13:34:34 +0000

    Day 5 part 2

--- day5.py
+++ day5.py
@@ -12,13 +12,36 @@ def lookup(inp: int, map_group: list[list[int]]):
 
 seeds = [int(x) for x in seeds.split(": ")[1].split(" ")]
 
+# keep for part 2
+seed_ranges = [(x, y) for x, y in zip(seeds[::2], seeds[1::2])]
 
 maps = [
-    [[int(k) for k in x.split()] for x in m.split(":")[1].strip().split("\n")]
-    for m in maps[0].strip().split("\n\n")
+    [[int(k) for k in x.split()] for x in m.split(":")[1].strip().split("\n")] for m in maps[0].strip().split("\n\n")
 ]
 
 for map_group in maps:
     seeds = [lookup(x, map_group) for x in seeds]
 
 print(min(seeds))
+
+
+def lookup_range(seed_ranges, maps):
+    for start, length in seed_ranges:
+        while length > 0:
+            for destination, source, distance in maps:
+                delta = start - source
+                if delta in range(distance):
+                    distance = min(distance - delta, length)
+                    yield (destination + delta, distance)
+                    start += distance
+                    length -= distance
+                    break
+            else:
+                yield start, length
+                break
+
+
+for map_group in maps:
+    seed_ranges = list(lookup_range(seed_ranges, map_group))
+
+print(min([d for d, l in seed_ranges]))
